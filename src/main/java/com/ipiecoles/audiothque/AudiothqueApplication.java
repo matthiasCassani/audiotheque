f3c31b3af01fef56835923a87eac325c27c650f7
Cassani <m.cassani@semeru.fayat.com>
2021-12-13 17:03:04 +0100

    first commit

---
+++ src/main/java/com/ipiecoles/audiothque/AudiothqueApplication.java
@@ -0,0 +1,13 @@
+package com.ipiecoles.audiothque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiothqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiothqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipiecoles/audiothque/AudiothqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipiecoles.audiothque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiothqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
